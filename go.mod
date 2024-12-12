deabd7765053cb78211e62c850aca855ee52bcb7
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-12-12 15:19:43 +0530

    feat: basic scraper

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kartikm7/scrapo
 
 go 1.23.4
+
+require golang.org/x/net v0.32.0
--- scrapo.go
+++ scrapo.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/kartikm7/scrapo/parser"
+	"github.com/kartikm7/scrapo/scraper"
+)
+
 func main() {
+	if html, err := scraper.Scraper("https://www.llocal.in"); err == nil {
+		if cleaned, err := parser.Parser(html); err == nil {
+			fmt.Println(cleaned)
+		}
+	} else {
+		fmt.Println("Dude some error occured")
+		fmt.Println(err)
+	}
 }

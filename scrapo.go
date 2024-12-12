33f88c6dea0970a7782b2dbf805a71aa84107f8e
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-12-12 17:11:29 +0530

    feat: multi-threaded scraper

--- scrapo.go
+++ scrapo.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/kartikm7/scrapo/parser"
 	"github.com/kartikm7/scrapo/scraper"
 )
 
+// type for the website array/slice idk the difference yet
+type website struct {
+	name string
+	url  string
+}
+
+// wait group which acts basically like a counter
+var wg = sync.WaitGroup{}
+
+// mutual exclusion
+var mutex = sync.Mutex{}
+
+// this is mock data to test go routines
+var websites []website = []website{
+	{"LLocal", "https://www.llocal.in"},
+	{"DocNext", "https://docnext.llocal.in"},
+	{"Ono", "https://ono.llocal.in"},
+	{"My GitHub", "https://www.github.com/kartikm7"},
+}
+
+var result = []string{}
+
 func main() {
-	if html, err := scraper.Scraper("https://www.llocal.in"); err == nil {
-		if cleaned, err := parser.Parser(html); err == nil {
-			fmt.Println(cleaned)
-		}
-	} else {
-		fmt.Println("Dude some error occured")
-		fmt.Println(err)
+	startingTime := time.Now()
+	for _, val := range websites {
+
+		// increment the counter
+		wg.Add(1)
+		go func() {
+			if data, err := scraper.Scraper(val.url); err == nil {
+				// I don't quite care about the scraped data as much as I do with the order of processes
+				if _, err := parser.Parser(data); err == nil {
+					mutex.Lock()
+					result = append(result, val.name)
+					mutex.Unlock()
+				}
+			}
+			// decrements the counter by 1
+			wg.Done()
+		}()
+		// it waits here till the counter hits 0!
+		wg.Wait()
 	}
+	fmt.Printf("The results: %v\n", result)
+	fmt.Println("Time since execution: ", time.Since(startingTime))
 }

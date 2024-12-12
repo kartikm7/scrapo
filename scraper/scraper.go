2336240d43d6c61ff0fd226d586b1d93dcf63a86
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-12-12 15:19:07 +0530

    feat: single link scraper

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,4 +1,20 @@
 package scraper
 
-func Scraper(links []string) {
+import (
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+func Scraper(url string) (*html.Node, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	body, err := html.Parse(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
